5cd16e0de3bb2f3e74d944831695fd93e7f28594
Tiny <tinysilk@hotmail.com>
2016-03-01 14:44:38 +0800

    初步确定大家决定做一个塔防游戏.

---
+++ library/imports/dc/dcd0125b-03f0-4037-8c97-6ebd503b7b88.js
@@ -0,0 +1,42 @@
+"use strict";
+
+cc.Class({
+    "extends": cc.Component,
+
+    properties: {
+        // foo: {
+        //    default: null,
+        //    url: cc.Texture2D,  // optional, default is typeof default
+        //    serializable: true, // optional, default is true
+        //    visible: true,      // optional, default is true
+        //    displayName: 'Foo', // optional
+        //    readonly: false,    // optional, default is false
+        // },
+        // ...
+        startBtn: {
+            "default": null,
+            type: cc.Node
+        },
+        guanyu: {
+            "default": null,
+            type: cc.Node
+        },
+        title: {
+            "default": null,
+            type: cc.Node
+        }
+    },
+
+    // use this for initialization
+    onLoad: function onLoad() {
+        this.guanyu.runAction(cc.repeatForever(cc.sequence(cc.moveTo(2, cc.p(-330, -221)), cc.scaleTo(0.5, -1, 1), cc.moveTo(2, cc.p(330, -221)), cc.scaleTo(0.5, 1, 1))));
+        this.title.runAction(cc.repeatForever(cc.sequence(cc.rotateTo(0.8, 10), cc.rotateTo(1.6, -20), cc.rotateTo(0.8, 10))));
+    },
+
+    onClick: function onClick() {}
+
+    // called every frame, uncomment this function to activate update callback
+    // update: function (dt) {
+
+    // },
+});
